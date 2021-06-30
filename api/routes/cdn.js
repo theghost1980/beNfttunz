aea6690db829794ad457db75242a25df87758252
Saturno Mangieri <saturnob612@gmail.com>
2021-06-30 15:03:41 -0400

    added a fix to allow use the original file name when uploading to cdn, testing live now

--- api/routes/cdn.js
+++ api/routes/cdn.js
@@ -61,6 +61,7 @@ router.get('/cdndata', authToken, function(req,res){
 
 /////upload test 
 router.post('/upload', authToken, function(req,res){
+    // TODO here: we must test if we may need the overwrite a file and how do we check on FE.
     const username = req.query.username; 
     if(!username){ return res.status(400).send({ status: 'failed', message: 'Missing Query!' })};
     const baseFolder = username;
@@ -68,7 +69,7 @@ router.post('/upload', authToken, function(req,res){
         if(err) return res.status(500).send({ status: 'failed', message: 'Error on Multer.', error: err });
         const { fileType, date } = req.body;
         let path = baseFolder + `/${fileType}`;
-        const options = fileType === 'image' ? { folder: path, } : { folder: path, resource_type: 'video' }; //for now we treat the rest as video.
+        const options = fileType === 'image' ? { folder: path, use_filename: true } : { folder: path, use_filename: true, resource_type: 'video' }; //for now we treat the rest as video.
         console.log(`About to upload to ${path}.`);
         console.log(req.files); //now we will handle from FE between options: video, image, audio.
         let upPromise = req.files.map(file => new Promise((resolve,reject) => {
